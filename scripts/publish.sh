02025c230c92675e5123c31af6e3cf286708a4a8
Ioannis Zannos <zannos@gmail.com>
2014-10-11 21:33:05 +0300

    Saved on: Sat Oct 11 21:33:05 EEST 2014

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,6 @@
 tiddlywiki ../readonly --rendertiddler $:/core/save/all ../../index.html text/plain
 git checkout master
+cd ../
 git add .
 git commit -am "Saved on: `date`"
 git push origin master
@@ -7,4 +8,4 @@ git checkout gh-pages
 git merge master
 git push origin gh-pages
 git checkout master
-rsync -avz ../readonly/output/index.html iani@larigot.avarts.ionio.gr:/Library/WebServer/Documents/users/iani/wikis/tw5square.html
+rsync -avz index.html iani@larigot.avarts.ionio.gr:/Library/WebServer/Documents/users/iani/wikis/tw5square.html
